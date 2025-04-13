dffd2ca2ff1a533e7ad9b613547a0899275591d2
Renato Rak <renatorak20@gmail.com>
2025-04-13 10:57:50 +0200

    Added servo and temperature sensor

--- sketch.ino
+++ sketch.ino
@@ -1,5 +1,6 @@
 #include <Keypad.h>
 #include <LiquidCrystal.h>
+#include <Servo.h>
 
 LiquidCrystal lcd(12, 11, 10, A5, A4, A3);
 
@@ -16,6 +17,7 @@ uint8_t colPins[COLS] = {5, 4, 3, 2};
 uint8_t rowPins[ROWS] = {9, 8, 7, 6};
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+Servo servo;
 
 String correctPin = "1234";
 String enteredPin = "";
@@ -25,8 +27,13 @@ String resetPasswordPattern = "*201*";
 volatile bool isResetPasswordMode = false;
 String inputPassword = "";
 
+const int ntcPin = A0;
+const int servoPin = 13;
+
 void setup()
 {
+  servo.attach(servoPin);
+  servo.write(-90);
   lcd.begin(20, 4);
   lcd.setCursor(0, 0);
   lcd.print("Dobrodosli!");
@@ -79,18 +86,37 @@ void checkPin()
 
   if (enteredPin == correctPin)
   {
+  float temp = readTemperature();
+  Serial.println(temp);
+  if (temp > 38.0 || temp < 0.0)
+  {
+    lcd.setCursor(0, 0);
+    lcd.print("Temp: ");
+    lcd.print(temp, 1);
+    lcd.setCursor(0, 1);
+    lcd.print("Ne moze se otvoriti");
+    lcd.setCursor(0, 2);
+    lcd.print("Temp izvan granica");
+    delay(3000);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Unesite PIN:");
+    enteredPin = "";
+    return;
+  } else {
     lcd.setCursor(0, 0);
     lcd.print("Tocan PIN!");
     lcd.setCursor(0, 1);
     lcd.print("Sef otvoren!");
     hasEntered = true;
-
+    unlock();
     delay(5000); // sef ostaje otvoren 5 sekundi
 
     // Zakljucavanje
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Sef zakljucan.");
+    lock();
     delay(2000);
 
     lcd.clear();
@@ -98,6 +124,7 @@ void checkPin()
     lcd.print("Unesite PIN:");
     hasEntered = false;
     enteredPin = "";
+  }
   }
   else if (enteredPin == resetPasswordPattern)
   {
@@ -184,4 +211,20 @@ void newPin()
       lcd.print("        ");
     }
   }
+}
+
+float readTemperature()
+{
+  const float BETA = 3950;
+  int analogValue = analogRead(ntcPin);
+  float celsius = 1 / (log(1 / (1023. / analogValue - 1)) / BETA + 1.0 / 298.15) - 273.15;
+  return celsius;
+}
+
+void unlock() {
+  servo.write(180);
+}
+
+void lock() {
+  servo.write(-90);
 }
